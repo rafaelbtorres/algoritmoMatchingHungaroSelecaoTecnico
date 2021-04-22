8bffd8bf28baec48934585cd58f0b4a0a941577d
Rafael Torres <dasd>
2021-04-22 16:13:05 -0300

    merge de branchs

--- src/algoritmoselecaodetecnico/Grafico.java
+++ src/algoritmoselecaodetecnico/Grafico.java
@@ -25,44 +25,20 @@ public class Grafico {
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
 
         for (int i = 0; i < medicoesComplexidade.size(); i++) {
-            dataSet.setValue(medicoesComplexidade.get(i).getTempo(), "I7 Com complexidade", "" + medicoesComplexidade.get(i).getQuantidadeExecucoes());
+            dataSet.setValue(medicoesComplexidade.get(i).getTempo(), "I5 Com complexidade", "" + medicoesComplexidade.get(i).getQuantidadeExecucoes());
         }
         for (int i = 0; i < medicoesSemComplexidade.size(); i++) {
-            dataSet.setValue(medicoesSemComplexidade.get(i).getTempo(), "I7 Sem complexidade", "" + medicoesSemComplexidade.get(i).getQuantidadeExecucoes());
-        }        
-//        dataSet.setValue(0.0, "I7 Com complexidade", "Alegria");
-//        dataSet.setValue(100.0, "I7 Com complexidade", "Brilho");
-//        dataSet.setValue(200.0, "I7 Com complexidade", "Aço");
-//        dataSet.setValue(300.0, "I7 Com complexidade", "Sentido?");
-
-//        dataSet.setValue(0.0, "Row 2", "Alegria");
-//        dataSet.setValue(50.0, "Row 2", "Brilho");
-//        dataSet.setValue(100.0, "Row 2", "Aço");
-//        dataSet.setValue(300.0, "Row 2", "Sentido?");
-//
-//        dataSet.setValue(0.0, "Row 3", "Alegria");
-//        dataSet.setValue(90.0, "Row 3", "Brilho");
-//        dataSet.setValue(120.0, "Row 3", "Aço");
-//        dataSet.setValue(300.0, "Row 3", "Sentido?");
-//
-//        dataSet.setValue(0.0, "Row 4", "Alegria");
-//        dataSet.setValue(10.0, "Row 4", "Brilho");
-//        dataSet.setValue(250.0, "Row 4", "Aço");
-//        dataSet.setValue(300.0, "Row 4", "Sentido?");
-
-//        for(Pessoa pessoa : listaDePessoas){
-//            dataSet.addValue(pessoa.getIdade(), pessoa.getNome(), "");
-//        }
-        
+            dataSet.setValue(medicoesSemComplexidade.get(i).getTempo(), "I5 Sem complexidade", "" + medicoesSemComplexidade.get(i).getQuantidadeExecucoes());
+        }                
         return dataSet;
 
     }
 
     public JFreeChart createLineChart(CategoryDataset dataSet) {
         JFreeChart grafico = ChartFactory.createLineChart(
-                "Os guri do Mansilha",
-                "",
-                "Ceva chegando na mente",
+                "Algoritmo Matching Hungaro",
+                "Tamanho de instância (n)",
+                "Tempo de execução (t)",
                 dataSet,
                 PlotOrientation.VERTICAL,
                 true,
