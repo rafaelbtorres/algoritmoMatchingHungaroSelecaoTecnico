2cc8b61c6bf6858532cc419f5c1edc214c7281fb
Rafael Torres <dasd>
2021-04-27 19:29:14 -0300

    feito plotagem de pontos no grafico

--- src/algoritmoselecaodetecnico/Grafico.java
+++ src/algoritmoselecaodetecnico/Grafico.java
@@ -5,12 +5,18 @@
  */
 package algoritmoselecaodetecnico;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import org.jfree.chart.ChartFactory;
+import static org.jfree.chart.ChartFactory.createLineChart;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.DatasetRenderingOrder;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
@@ -33,32 +39,43 @@ public class Grafico {
         return dataSet;
 
     }
-
-    public JFreeChart createLineChart(CategoryDataset dataSet) {
-        JFreeChart grafico = ChartFactory.createLineChart(
-                "Algoritmo Matching Hungaro",
-                "Tamanho de instância (n)",
-                "Tempo de execução (t)",
-                dataSet,
-                PlotOrientation.VERTICAL,
-                true,
-                false,
-                false);
-
-        return grafico;
+    
+        public JFreeChart createLineChart(CategoryDataset dataSet) {
+            final JFreeChart grafico = ChartFactory.createLineChart(
+                    "Algoritmo Matching Hungaro",
+                    "Tamanho de instância (n)",
+                    "Tempo de execução (t)",
+                    dataSet,
+                    PlotOrientation.VERTICAL,
+                    false,
+                    false,
+                    false);
+            return grafico;
     }
-
-
+        
+    
+        
+        
     public ChartPanel criarGrafico(ArrayList<Medicao> medicoesComplexidade, ArrayList<Medicao> medicoesSemComplexidade) {
 
+        
+        
         CategoryDataset dataSet = this.createDataSet(medicoesComplexidade, medicoesSemComplexidade);
-
+        
         JFreeChart grafico = this.createLineChart(dataSet);
+        
+        //Grafico de Bolinha
+        final CategoryPlot plot = grafico.getCategoryPlot();
+        final CategoryItemRenderer renderer2 = new LineAndShapeRenderer();
+                renderer2.setSeriesPaint(0, Color.blue);
+                plot.setRenderer(1, renderer2);
+                plot.setDatasetRenderingOrder(DatasetRenderingOrder.REVERSE);
+                plot.setDataset(1, dataSet);
 
         ChartPanel painelDoGrafico = new ChartPanel(grafico);
         painelDoGrafico.setPreferredSize(new Dimension(400, 400));
 
         return painelDoGrafico;
     }
-
+    
 }
